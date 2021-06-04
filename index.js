ce4209af47d8158dbbdd79252edc28627eeec0c8
Samuel-Anumudu <you@example.com>
2021-06-04 05:56:42 +0200

    initial commit

--- index.js
+++ index.js
@@ -8,11 +8,36 @@ AOS.init({
 const nav = document.querySelector(".def__nav");
 const navUl = document.querySelector(".nav-ul");
 const menuBtn = document.querySelector(".menu-btn");
+const submit = document.querySelector(".submit");
+const input = document.querySelector(".input");
+const required1 = document.querySelector(".require");
+const required2 = document.querySelector(".require2");
+const required3 = document.querySelector(".require3");
 const firstLi = $("ul.nav-ul li").eq(1);
 const secondLi = $("ul.nav-ul li").eq(2);
 const thirdLi = $("ul.nav-ul li").eq(3);
 const fourthLi = $("ul.nav-ul li").eq(4);
 const fifthLi = $("ul.nav-ul li").eq(5);
+
+// Prevent defualt behavior of form submission onclick and display required
+function displayRequiredFormData() {
+  required1.style.display = "block";
+  required2.style.display = "block";
+  required3.style.display = "block";
+}
+
+submit.onclick = function (e) {
+  e.preventDefault();
+  if (document.forms["form"].name.value === "") {
+    displayRequiredFormData();
+  } else if (document.forms["form"].email.value === "") {
+    displayRequiredFormData();
+  } else if (document.forms["form"].phone.value === "") {
+    displayRequiredFormData();
+  } else {
+    return true;
+  }
+};
 
 // Add active class nav on scroll
 window.addEventListener("scroll", fixNav);
